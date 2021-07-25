388be04971cdddd2d3942ec835d1ff661755054e
AnatolySoroka <84930274+AnatolySoroka@users.noreply.github.com>
2021-07-25 22:11:22 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+  
+  $('.blog__slider').slick({
+    arrows: false,
+    dots: true,
+  })
+  
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').toggleClass('menu__list--active');
+  });
+  
+  var mixer = mixitup('.gallery__content');
+
+});
